64d37d75ab78a80a8caa4779a4d5347c25e66191
EthanWoe <woessner.8@wright.edu>
2025-11-06 14:18:13 -0500

    updated and Working

--- mtg-ui/src/app/app.routes.ts
+++ mtg-ui/src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '',   pathMatch: 'full', redirectTo: 'orders' },
+  { path: 'orders', loadComponent: () => import('/home/ethanwoe/cs4900-ui-mtg/mtg-ui/src/app/features/order/orders/orders.component').then(m => m.OrdersComponent) },
+  { path: '**', redirectTo: 'orders' }
+];
